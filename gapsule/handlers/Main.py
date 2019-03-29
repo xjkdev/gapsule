f703984cd2d8d3a11b80ef8592c88f4e7f3e2728
xjkdev <xjk2008@gmail.com>
2019-03-29 20:58:22 +0800

    feature: Decorator ajaxquery
    add a decorator in utils, which devides ajax query from normal GET query.

--- gapsule/handlers/Main.py
+++ gapsule/handlers/Main.py
@@ -3,5 +3,4 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        # self.write("hello world")
         self.render("index.html")
---
+++ gapsule/utils/__init__.py
@@ -0,0 +1,4 @@
+
+from gapsule.utils.decorators import ajaxquery
+
+__all__ = ['ajaxquery']
