54e562e17ceb1959f7f3da6fcc1abff1fa9bc5eb
huoli-knight <158906158@qq.com>
2019-04-02 20:50:32 +0800

    命名url

--- gapsule/urls.py
+++ gapsule/urls.py
@@ -4,6 +4,9 @@ import os
 from gapsule.handlers.Main import MainHandler
 from gapsule.handlers.User import Signin
 from gapsule.settings import settings
+from gapsule.handlers.RepoHandler import (CodeListHandler, FolderListHandler,
+                                          FileContentHandler,)
+#from gapsule.handlers.PullRequest import (PullRequestPageHandler,)
 
 routes = [
     (r"/", MainHandler),
@@ -11,5 +14,9 @@ routes = [
     (r"/static/(.*)", tornado.web.StaticFileHandler, {
         'path': settings['static_path']
     }),
+    (r"/?<username>(w+)/?<projectname>(w+)/", CodeListHandler),
+    (r"/?<username>(w+)/?<projectname>(w+)/tree/?<branch>(w+)/?<restpath>(^/]+)", FolderListHandler),
+    (r"/?<username>(w+)/?<projectname>(w+)/blob/?<branch>(w+)/?<restpath>(^/]+)", FileContentHandler),
+    #(r"/(w+)/(w+)/pulls", PullRequestPageHandler),
     (r"/.*", MainHandler),
 ]
