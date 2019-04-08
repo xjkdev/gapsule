a80eb3c394dc3dafc4492044eb79fee6b93e4207
huoli-knight <158906158@qq.com>
2019-04-08 23:00:23 +0800

    Repo 及url

--- gapsule/urls.py
+++ gapsule/urls.py
@@ -2,22 +2,19 @@ import tornado.web
 import os
 
 from gapsule.handlers.Main import MainHandler
-from gapsule.handlers.User import Signin, Signup
+from gapsule.handlers.User import Signin
 from gapsule.settings import settings
 from gapsule.handlers.RepoHandler import (CodeListHandler, FolderListHandler,
                                           FileContentHandler,)
-#from gapsule.handlers.PullRequest import (PullRequestPageHandler,)
 
 routes = [
     (r"/", MainHandler),
-    (r"/signin/?", Signin.SignInHandler),
-    (r"/signup(/verify|/finishing)?/?", Signup.SignUpHandler),
+    (r"/signin", Signin.SignInHandler),
     (r"/static/(.*)", tornado.web.StaticFileHandler, {
-        'path': settings['static_path']
+       'path': settings['static_path']
     }),
-    (r"/?<username>(w+)/?<projectname>(w+)/", CodeListHandler),
-    (r"/?<username>(w+)/?<projectname>(w+)/tree/?<branch>(w+)/?<restpath>(^/]+)", FolderListHandler),
-    (r"/?<username>(w+)/?<projectname>(w+)/blob/?<branch>(w+)/?<restpath>(^/]+)", FileContentHandler),
-    #(r"/(w+)/(w+)/pulls", PullRequestPageHandler),
+    (r"/(?P<username>\w+)/(?P<projectname>\w+)/?", CodeListHandler),
+    (r"/(?P<username>\w+)/(?P<projectname>\w+)/tree/(?P<branch>\w+)/(?P<restpath>.*)/?", FolderListHandler),
+    (r"/(?P<username>\w+)/(?P<projectname>\w+)/blob/(?P<branch>\w+)/(?P<restpath>.*)/?", FileContentHandler),
     (r"/.*", MainHandler),
 ]
