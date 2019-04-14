1d4ff459b10ce09d49156ae6faace93299def71a
MHaoYun <1023463276@qq.com>
2019-04-14 10:40:14 +0800

    Feature: add newrepo page

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -7,18 +7,19 @@
 
     <b-collapse id="navbarSupportedContent" is-nav>
       <b-navbar-nav class="mr-auto">
-        <b-nav-item active to="#">
-          Home
+        <b-nav-item to="#">
+          Pull requests
           <span class="sr-only">(current)</span>
         </b-nav-item>
-        <b-nav-item to="#">Link</b-nav-item>
-        <b-nav-item-dropdown text="Dropdown">
+        <b-nav-item to="#">Issues</b-nav-item>
+        <b-nav-item to="#">Marketplace</b-nav-item>
+        <!-- <b-nav-item-dropdown text="Dropdown">
           <b-dropdown-item to="#">Action</b-dropdown-item>
           <b-dropdown-item to="#">Another action</b-dropdown-item>
           <b-dropdown-divider/>
           <b-dropdown-item to="#">Something else here</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item to="#">Disabled</b-nav-item>
+        </b-nav-item-dropdown>-->
+        <b-nav-item to="#">Explore</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
@@ -31,10 +32,12 @@
           />
           <b-button variant="outline-success" class="my-2 my-sm-0" size="sm">Search</b-button>
         </b-nav-form>&nbsp;&nbsp;
-        <b-nav-item to="/signin" style="color: white">Sign in</b-nav-item>&nbsp;&nbsp;
+        <b-nav-item to="/signin" style="color: white" v-if="!username">Sign in</b-nav-item>&nbsp;&nbsp;
         <b-nav-form>
-          <b-button to="/signup" size="sm" class="my-2 my-sm-0">Sign Up</b-button>
+          <b-button to="/signup" size="sm" class="my-2 my-sm-0" v-if="!username">Sign Up</b-button>
         </b-nav-form>
+        <img v-if="icon" :src="icon" alt="UserIcon">
+        <span v-if="username">{{ username }}</span>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -46,6 +49,12 @@ export default {
   mounted() {
     // let togglebtn = document.getElementById("navbar-toggler");
     // let collapseinit = new Collapse(togglebtn);
+  },
+  data() {
+    return {
+      username: document.cookie.username,
+      icon: document.cookie.icon
+    };
   }
 };
 </script>
